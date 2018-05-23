a4204080cb13d59272028db095dd88a3046efde3
Admin <admin@Admins-Mac-mini.local>
2018-05-23 15:03:13 +0530

    Snake home screen

---
+++ snake/GameManager.swift
@@ -0,0 +1,12 @@
+//
+//  GameManager.swift
+//  snake
+//
+//  Created by Admin on 21/05/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+class GameManager {
+}
